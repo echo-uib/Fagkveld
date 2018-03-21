f10fe5636f66d365facdfbf2bf00c20d47627763
Ragnhild Aalvik <raa009@student.uib.no>
2018-03-21 16:32:46 +0100

    Done with event cards

--- fagkveld/src/bidrag.js
+++ fagkveld/src/bidrag.js
@@ -1,23 +1,35 @@
 const data = {
   "workshops": [
     {
-      "tittel":"Lag en enkel webapp i vue.js",
+      "tittel": "Lag en enkel webapp i vue.js,",
       "foredragsholder": "Trond Svanes, Stacc",
       "info": "Dette er en workshop for deg som elsker nye teknologier. Meld deg på og bli med på en reise inn i vue-land",
-      "klokkeslett":"17.15 - 19.00",
+      "klokkeslett": "17.15 - 19.00",
     },
     {
-      "tittel":"Lag en enkel webapp i react.js",
+      "tittel": "Lag en enkel webapp i react.js,",
       "foredragsholder": "Alexander Rosbach, Computas AS",
       "info": "Bygg en applikasjon i Facebook sitt eget rammeverk, React. Et absolutt must for moderne webutviklere!",
-      "klokkeslett":"17.15 - 19.00",
+      "klokkeslett": "17.15 - 19.00",
     },
   ],
   "foredrag": [
     {
-      "tittel": "Maskinlæring",
+      "tittel": "Maskinlæring,",
       "foredragsholder": "Morten Øvrebø, BEKK",
       "info": "Placeholder tekst med info om foredraget.",
+      "klokkeslett": "17.15 - 19.00",
+    },
+    {
+      "tittel": "Hvorfor jeg kastet Ruby on Rails,",
+      "foredragsholder": "Knut Anders Stokke",
+      "info": "Jeg tok over et ræævdårlig system som hadde brukt Rails. Jeg klikket! Nå klikker jeg ikke lengre, jeg elm'er",
+      "klokkeslett": "17.15 - 19.00",
+    },
+    {
+      "tittel": "AI-en min har har begynt å ikke-tape mot meg,",
+      "foredragsholder": "Morten Lohne",
+      "info": "Sjakkens historie spenner over et tidsrom på ca 1500 år. De tidligste forløperne antas å ha oppstått i India rundt år 500, før den kom via Persia til Europa. Frasen 'sjakk matt' kommer fra det persiske uttrykket shâh mâh som betyr kongen er beseiret eller kongen er hjelpeløs. I Europa utviklet spillet seg til den formen det har i dag en gang på 1500-tallet. Moderne sjakkturneringer som man kjenner til dem i dag startet først i andre halvdel av 1800-tallet.",
       "klokkeslett": "17.15 - 19.00",
     }
   ],
