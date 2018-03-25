18ee97a3b1269cc7ec0f1d08bdeb9e45ca0f7c83
Kristian Rosland <kristian.rosland@gmail.com>
2018-03-25 16:56:12 +0200

    Edit TopInfoText

--- fagkveld/src/bidrag.js
+++ fagkveld/src/bidrag.js
@@ -1,39 +1,29 @@
 const data = {
+  "topText": "Velkommen til fagkveld for IT-studenter i Bergen! Fagutvalget ved Institutt for Informatikk, Fagutvalget Infomedia og Linjeforeningen Root inviterer sammen med Den Norske Dataforening til en kveld fullstappet med foredrag, workshops, mat og drikke! Det blir flere parallelle spor med underholdning, så det skal være mulig å finne noe for alle, uansett nivå. Husk at det er begrenset med plasser på workshops, så meld deg på nå!",
+
   "workshops": [
     {
       "tittel": "Lag en enkel webapp i vue.js,",
       "foredragsholder": "Trond Svanes, Stacc",
       "info": "Dette er en workshop for deg som elsker nye teknologier. Meld deg på og bli med på en reise inn i vue-land",
       "klokkeslett": "17.15 - 19.00",
-      "link": "www.google.com",
+      "link": "https://goo.gl/forms/a5Hat9A698120qZt2",
     },
     {
       "tittel": "Lag en enkel webapp i react.js,",
       "foredragsholder": "Alexander Rosbach, Computas AS",
       "info": "Bygg en applikasjon i Facebook sitt eget rammeverk, React. Et absolutt must for moderne webutviklere!",
       "klokkeslett": "17.15 - 19.00",
-      "link": "www.vg.no",
+      "link": "https://goo.gl/forms/a5Hat9A698120qZt2",
     },
   ],
   "foredrag": [
     {
-      "tittel": "Maskinlæring,",
-      "foredragsholder": "Morten Øvrebø, BEKK",
-      "info": "Placeholder tekst med info om foredraget.",
-      "klokkeslett": "17.15 - 19.00",
+      "tittel": "Hvordan er egentlig arbeidslivet som nyudannet?",
+      "foredragsholder": "Andreas Dukstad, Sparebanken Vest",
+      "info": "Mange er ofte nervøs for å starte arbeidslivet som nyutdannet. 'Er jeg flink nok?', 'Har jeg det som trengs?'. Hvordan er egentlig arbeidslivet sammenlignet med det man lærer på skolen? Dette er en erfaringsrapport fra to utviklere som satt med nøyaktig de samme spørsmålene for kun et par år siden",
+      "klokkeslett": "17.15 - 17.45",
     },
-    {
-      "tittel": "Hvorfor jeg kastet Ruby on Rails,",
-      "foredragsholder": "Knut Anders Stokke",
-      "info": "Jeg tok over et ræævdårlig system som hadde brukt Rails. Jeg klikket! Nå klikker jeg ikke lengre, jeg elm'er",
-      "klokkeslett": "17.15 - 19.00",
-    },
-    {
-      "tittel": "AI-en min har har begynt å ikke-tape mot meg,",
-      "foredragsholder": "Morten Lohne",
-      "info": "Sjakkens historie spenner over et tidsrom på ca 1500 år. De tidligste forløperne antas å ha oppstått i India rundt år 500, før den kom via Persia til Europa. Frasen 'sjakk matt' kommer fra det persiske uttrykket shâh mâh som betyr kongen er beseiret eller kongen er hjelpeløs. I Europa utviklet spillet seg til den formen det har i dag en gang på 1500-tallet. Moderne sjakkturneringer som man kjenner til dem i dag startet først i andre halvdel av 1800-tallet.",
-      "klokkeslett": "17.15 - 19.00",
-    }
   ],
 };
 
