7b4bb03c4ba40e597a85f357b1f2a14e1e8ee199
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-19 00:08:24 +0100

    Update service worker file, refactor accordingly

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./styles/css/index.css";
 import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
+import * as serviceWorker from "./serviceWorker";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-registerServiceWorker();
+serviceWorker.unregister();
