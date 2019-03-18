c71f1787ed8da2cff2c5fc2ed5d2c22412907507
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-18 22:58:09 +0100

    Remove react-text-truncate, I hate it

--- src/EventCard.js
+++ src/EventCard.js
@@ -1,18 +1,13 @@
 import React, { Component } from "react";
 import TimeLabel from "./TimeLabel";
 import SignUpLink from "./SignUpLink";
-import TextTruncate from "react-text-truncate";
 
 class EventCard extends Component {
   render() {
     const event = this.props.event;
     const type = this.props.type;
 
-    const title = event.title;
-    const time = event.time;
-    const speakers = event.speakers;
-    const info = event.info;
-    const lines = this.props.expand ? 0 : 2;
+    const { title, time, speakers, info } = event;
 
     let icon =
       title === "Pause med mat og drikke"
@@ -23,7 +18,6 @@ class EventCard extends Component {
 
     const timeLabel = <TimeLabel icon={icon} time={time} />;
     const registration = event.link ? <SignUpLink link={event.link} /> : null;
-    const onClick = () => this.props.onClick(event);
 
     return (
       <div className="event-card">
@@ -31,9 +25,7 @@ class EventCard extends Component {
         <div className="event-card-content">
           <p className="event-card-title"> {title} </p>
           <p className="event-card-author"> {speakers} </p>
-          <div className="truncate-wrapper" onClick={onClick}>
-            <TextTruncate line={lines} truncateText="..." text={info} />
-          </div>
+          <div className="truncate-wrapper">{info}</div>
           {registration}
         </div>
       </div>
