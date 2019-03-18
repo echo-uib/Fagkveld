cb6ba5fb7fb2e9d66e291a50151c03e2ddb633a7
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-18 21:14:59 +0100

    Add some particles to the background

--- src/App.js
+++ src/App.js
@@ -1,27 +1,51 @@
-import React, { Component } from 'react';
-import MainContainer from './MainContainer';
-import TopInfoBox from './TopInfoBox';
-import data from './bidrag.js';
+import React, { Component } from "react";
+import MainContainer from "./MainContainer";
+import TopInfoBox from "./TopInfoBox";
+import data from "./bidrag.js";
+import { Particles } from "react-particles-js";
 
 class App extends Component {
   render() {
     const comparator = (e1, e2) => {
-      const h1 = parseInt(e1.klokkeslett.substring(0,2), 10);
-      const h2 = parseInt(e2.klokkeslett.substring(0,2), 10);
-      const m1 = parseInt(e1.klokkeslett.substring(3,5), 10);
-      const m2 = parseInt(e2.klokkeslett.substring(3,5), 10);
+      const h1 = parseInt(e1.klokkeslett.substring(0, 2), 10);
+      const h2 = parseInt(e2.klokkeslett.substring(0, 2), 10);
+      const m1 = parseInt(e1.klokkeslett.substring(3, 5), 10);
+      const m2 = parseInt(e2.klokkeslett.substring(3, 5), 10);
 
-      if (h1 !== h2) return h1 - h2
-      else return m1 - m2
-    }
+      if (h1 !== h2) return h1 - h2;
+      else return m1 - m2;
+    };
 
-    data.foredrag.sort(comparator)
-    data.workshops.sort(comparator)
+    data.foredrag.sort(comparator);
+    data.workshops.sort(comparator);
 
     return (
-      <div className="app">
-        <TopInfoBox text={data.topText} />
-        <MainContainer data={data} />
+      <div>
+        <Particles
+          params={{
+            particles: {
+              number: {
+                value: 100
+              },
+              size: {
+                value: 3
+              }
+            },
+            interactivity: {
+              events: {
+                onhover: {
+                  enable: true,
+                  mode: "repulse"
+                }
+              }
+            }
+          }}
+          className="particles"
+        />
+        <div className="app">
+          <TopInfoBox text={data.topText} />
+          <MainContainer data={data} />
+        </div>
       </div>
     );
   }
