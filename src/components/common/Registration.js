81efb0276314ad5e860616ae097ed0204de8ea09
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-19 00:10:55 +0100

    Use const functions, they are cooler

--- src/components/common/Registration.js
+++ src/components/common/Registration.js
@@ -5,10 +5,10 @@ const SignUp = styled.a`
   font-size: 15px;
 `;
 
-export function SignUpLink({ link, text }) {
+export const SignUpLink = ({ link, text }) => {
   return (
     <SignUp href={link} target="_blank" rel="noopener noreferrer">
       {text}
     </SignUp>
   );
-}
+};
