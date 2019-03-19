77ee08108ce75ce8ac0eb90fd6ddf4278aa3c942
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-19 21:52:09 +0100

    Add remaining workshops, talks etc

--- src/Content.js
+++ src/Content.js
@@ -2,7 +2,7 @@ export const meta = {
   name: "Fagkveld",
   date: "29. mars kl. 17.00 | Media City Bergen",
   info:
-    "Velkommen til fagkveld 2019 for IT-studenter i Bergen! echo ved Institutt for Informatikk, Fagutvalget Infomedia og Linjeforeningen Root inviterer til en kveld fullstappet med foredrag, workshops, 🍕og 🍻! Ti forskjellige bedrifter har bidratt med sine beste foredragsholdere for å gi oss et innblikk i problemene og teknologiene de jobber med til daglig. Det vil bli parallelle spor med foredrag og workshops, så det skal være mulig å finne noe for alle, uansett nivå. Husk at det er begrenset antall plasser på workshops, så meld deg på nå!",
+    "Velkommen til fagkveld 2019 for IT-studenter i Bergen! echo ved Institutt for Informatikk, Fagutvalget Infomedia og Linjeforeningen Root inviterer til en kveld fullstappet med foredrag, workshops, 🍕og 🍻! 13 forskjellige bedrifter har bidratt med sine beste foredragsholdere for å gi oss et innblikk i problemene og teknologiene de jobber med til daglig. Det vil bli parallelle spor med foredrag og workshops, så det skal være mulig å finne noe for alle, uansett nivå. Husk at det er begrenset antall plasser på workshops, så meld deg på nå!",
   madeBy:
     "Laget av Ragnhild Aalvik og Kristian Rosland, oppdatert av Sondre Nilsen",
   registration: {
@@ -15,7 +15,7 @@ export const meta = {
       link: "#"
     },
     fim: {
-      text: "Påmelding FIM",
+      text: "Påmelding InfoMedia",
       link: "#"
     }
   }
@@ -25,14 +25,21 @@ export const workshops = [
     title: "Pause med mat og drikke",
     speakers: "",
     info: "",
-    time: "18.45 - 19.30"
+    time: "19.00 - 19.30"
   },
   {
     title: "CV-Workshop",
     speakers: "Adrian Moksness & Margrethe Johnsen, Netlight",
     info:
       "Hvordan bygger man en god CV for å få spennende muligheter etter studiet? Adrian og Margrethe i Netlight gir et innblikk i karrieremulighetene i Netlight, hva vi anser som en «god CV» og hvordan en kan jobbe med å bygge CVen sin gjennom erfaringer underveis i studietiden.",
-    time: "00.00 - 01.30",
+    time: "17.00 - 18.30",
+    link: "#"
+  },
+  {
+    title: "Hololens Workshop",
+    speakers: "Gry, Bouvet",
+    info: "TBA",
+    time: "17.00 - 18.30",
     link: "#"
   },
   {
@@ -40,7 +47,14 @@ export const workshops = [
     speakers: "Anders Linge Olsvik Breilid, Kantega",
     info:
       "I denne workshopen vil deltakerne lære å bruke biblioteket Leaflet for kartvisning i HTML med Javascript, samt lære hvordan man kan hente data fra et åpent API og visualisere dataene i kartet. Det vil være fokus på å bruke dokumentasjon aktivt for å forstå hvordan biblioteket og API-et fungerer.",
-    time: "00.00 - 01.30",
+    time: "19.30 - 21.00",
+    link: "#"
+  },
+  {
+    title: "TBA",
+    speakers: "StormGeo",
+    info: "TBA",
+    time: "19.30 - 21.00",
     link: "#"
   }
 ];
@@ -49,26 +63,74 @@ export const talks = [
     title: "Pause med mat og drikke",
     speakers: "",
     info: "",
-    time: "18.45 - 19.30"
+    time: "19.00 - 19.30"
   },
   {
     title: "Velkommen",
     speakers: "",
     info: "",
-    time: "17.00 - 17.15"
+    time: "17.00 - 17.10"
+  },
+  {
+    title: "Moderne teknikker for språkanalyse",
+    speakers: "Alexander Lundervold, HVL",
+    info:
+      "Moderne teknikker for språkanalyse (deep learning), samt fortelle litt generelt om hvordan ML/AI brukes innen helsevesenet.",
+    time: "17.10 - 17.40"
+  },
+  {
+    title:
+      "How we built the Netflix of sports and laid the groundwork to serve Tomorrow’s traffic",
+    speakers: "Thelo Gaultier, Vimond",
+    info:
+      "Kayo Sport is an Australian service streaming all kinds of Live and On-demand sport events, from international football games to light sabre competitions - they got them all. In this talk, we will go through what it takes to build a platform handling millions of concurrent users and keep it up and running through the storm.",
+    time: "17.40 - 18.00"
+  },
+  {
+    title: "Global Oil & Gas Software Deliveries",
+    speakers: "Carolina Sola og Andreas Kleiven, Tieto",
+    info:
+      "Tieto Oil & Gas is the globally leading provider of logistics management software designed to accommodate the diverse needs of the international oil and gas industry. Tieto developed the worlds first ever oil & gas industry logistics hub/collaborative tool in 2001, called DaWinci, and has since expanded to support business operations for our customers in more than 25 countries around the world.",
+    time: "18.00 - 18.20"
+  },
+  {
+    title: "Git 201",
+    speakers: "Thomas Wingsternes, Ambita",
+    info:
+      "Hvordan fungerer git? Hva er egentlig en 'commit', og hva er en 'branch'? Vi ser nærmere på hvordan enkelte git-kommandoer påvirker commit-historikken til et repository. Foredraget egner seg for folk som allerede bruker git, men som ikke har peiling på hva de holder på med",
+    time: "18.20 - 18.40"
+  },
+  {
+    title: "TBA",
+    speakers: "Knowit",
+    info: "TBA",
+    time: "18.40 - 19.00"
+  },
+  {
+    title: "Clean Code",
+    speakers: "Monica Køller, Computas",
+    info: "TBA",
+    time: "19.30 - 19.50"
   },
   {
     title: "Vil du jobbe med data?",
     speakers: "Didrik Finnøy, Data Scientist hos Atea",
     info:
       "Data er den nye elektrisiteten, den nye oljen, og kanskje den mest spennende karriereretningen man kan velge i disse dager. Foredraget gir innsikt i hvordan det er å jobbe med data i Bergen / Norge, og  hvordan man skiller seg ut når man søker data - relaterte jobber. Du får høre litt om hva slags arbeid som venter deg, og hva som kreves for å lykkes. Didrik Finnøy, Data Scientist hos Atea, studerte 'feil' fag på universitetet, og har kjempet seg inn i data bransjen gjennom kurs, bøker, og personlige prosjekter. I dag jobber han som konsulent, og hjelper Atea sine kunder med å utvinne verdi fra dataen de forvalter.",
-    time: "00.00 - 00.20"
+    time: "19.50 - 20.10"
   },
   {
     title: "Hvordan ta i bruk web plattformen for bedre kodedeling?",
     speakers: "Øystein Østebø Olsen og Torbjørn Angeltveit, Scout Gaming",
     info:
       "Custom elements er en web standard som endelig beskriver hvordan rammeverk kan snakke sammen og en viktig primitiv for å endelig kunne fokusere på å lage gode UI elementer. En introduksjon til hva det er og hvordan det hjelper rammeverk å snakke samme språk.",
-    time: "00.20 - 00.40"
+    time: "20.10 - 20.30"
+  },
+  {
+    title:
+      "Hva skjer når en bruker klikker på feil lenke og det sitter en avansert trusselaktør i andre enden?",
+    speakers: "Martin B. Nordby, mnemonic",
+    info: "TBA",
+    time: "20.30 - 21.00"
   }
 ];
