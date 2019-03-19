315b6776a8e62bd843506a04a77b880a720883f3
Sondre Nilsen <nilsen.sondre@gmail.com>
2019-03-19 20:14:37 +0100

    Refactor TimeLabel to use styled-components

--- src/components/common/TimeLabel.js
+++ src/components/common/TimeLabel.js
@@ -1,15 +1,31 @@
 import React from "react";
+import styled from "styled-components";
 import FontAwesome from "react-fontawesome";
+
+const TimeWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: row;
+  margin-left: -35px;
+
+  @media (max-width: 978px) {
+    margin-left: 0;
+  }
+`;
+
+const TimeText = styled.p`
+  margin: 0;
+  margin-left: 15px;
+`;
 
 export const TimeLabel = props => {
   const iconStyle = { lineHeight: "1.30em", fontSize: "1.30em" };
   const icon = <FontAwesome name={props.icon} style={iconStyle} />;
-  const time = <p className="time-label-text"> {props.time} </p>;
 
   return (
-    <div className="time-label">
+    <TimeWrapper>
       {icon}
-      {time}
-    </div>
+      <TimeText>{props.time}</TimeText>
+    </TimeWrapper>
   );
 };
